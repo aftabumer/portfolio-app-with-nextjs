5f19f439cf434e98067f6d828f271d54f80b8912
aftabumer <innocentboy0300@gmail.com>
2020-11-30 21:49:54 +0500

    -get data from id.

--- pages/portfolios/[portfolio_id]/index.js
+++ pages/portfolios/[portfolio_id]/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 import BaseLayout from "../../../components/layouts/BaseLayout";
+import axios from "axios";
 
-const PortfolioId = ({ portfolio_id }) => {
-  console.log(portfolio_id);
+const PortfolioId = ({ portfolio }) => {
   return (
     <BaseLayout>
       <h1>I am Portfolio Page</h1>
-      <h2>{portfolio_id}</h2>
+      <h1>{portfolio.title}</h1>
+      <p>Body: {portfolio.body}</p>
+      <p>Id: {portfolio.id}</p>
     </BaseLayout>
   );
 };
 
-PortfolioId.getInitialProps = (ctx) => {
-  debugger;
+PortfolioId.getInitialProps = async (ctx) => {
   const { portfolio_id } = ctx["query"];
-  return { portfolio_id };
+  const res = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${portfolio_id}`
+  );
+  const posts = res.data;
+  return { portfolio: posts };
 };
 
 export default PortfolioId;

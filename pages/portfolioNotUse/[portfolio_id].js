6aab408a1ffc145352f6a3c123f0353162032c55
aftabumer <innocentboy0300@gmail.com>
2020-12-01 18:33:36 +0500

    -create custom server using express.

--- pages/portfolioNotUse/[portfolio_id].js
+++ pages/portfolioNotUse/[portfolio_id].js
@@ -1,5 +1,5 @@
 import React from "react";
-import BaseLayout from "../../../components/layouts/BaseLayout";
+import BaseLayout from "../../components/layouts/BaseLayout";
 import axios from "axios";
 
 const PortfolioId = ({ portfolio }) => {
